613a800a798020fc381a320e9a6b1f5336c80f34
Иван <cvb@sdf>
2023-11-13 00:17:01 +0300

    finish create animation

--- app/src/main/java/com/fortetigre/gamibrjo/MainActivity.kt
+++ app/src/main/java/com/fortetigre/gamibrjo/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.fortetigre.gamibrjo
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ActionMode
 import android.view.View
+import android.view.ViewPropertyAnimator
+import android.view.animation.Animation
 import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -32,7 +37,6 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             delay(1000)
             setupRandomCrystal()
-
         }
         setupCrystalClickListener()
     }
@@ -42,18 +46,22 @@ class MainActivity : AppCompatActivity() {
         ivCrystal.setOnClickListener {
             if (!isAnimationActive) {
                 isAnimationActive = true
+                val startX = ivCrystal.x
+                val startY = ivCrystal.y
                 launchSuccessAnim()
                 lifecycleScope.launch {
                     delay(510)
+                    launchBackAnimation(startX, startY)
                     setupRandomCrystal()
                 }
             }
         }
     }
 
+
     private fun setupRandomCrystal() {
         isAnimationActive = false
-        val img = if (Random.nextBoolean()) R.drawable.crystal_blue else R.drawable.crystal_blue_2
+        val img = R.drawable.crystal_blue
         ivCrystal.setImageDrawable(getDrawable(img))
         ivCrystal.rotation = (Random.nextFloat()) * 100
         var randomScale = (Random.nextDouble(2.0)).toFloat()
@@ -64,35 +72,62 @@ class MainActivity : AppCompatActivity() {
         val randomY = (Random.nextInt(yEnd.toInt() - ivCrystal.height)).toFloat()
         ivCrystal.x = randomX
         ivCrystal.y = randomY
+        Log.d("COORDINATE", "x:$randomX / y:$randomY")
         lifecycleScope.launch {
             delay(1000)
             if (!isAnimationActive) {
                 isAnimationActive = true
                 launchFailureAnim()
-                delay(510)
-                setupRandomCrystal()
+//                delay(510)
+//                launchBackAnimation(randomX, randomY)
+//                setupRandomCrystal()
             }
         }
     }
 
     private fun launchSuccessAnim() {
+        isAnimationActive=true
+        val startXCoordinate = ivCrystal.x
+        val startYCoordinate = ivCrystal.y
         ivCrystal.animate().apply {
             duration = 500
             translationX(xEnd)
             translationY(yStart)
             scaleX(0.1F)
             scaleY(0.1F)
+            withEndAction {
+                launchBackAnimation(startXCoordinate, startYCoordinate)
+            }
         }
     }
 
     private fun launchFailureAnim() {
+        val startXCoordinate = ivCrystal.x
+        val startYCoordinate = ivCrystal.y
+        isAnimationActive=true
         ivCrystal.animate().apply {
             duration = 500
             translationX(xFailure)
             translationY(yFailure)
             scaleX(0.1F)
             scaleY(0.1F)
+            withEndAction {
+                launchBackAnimation(startXCoordinate, startYCoordinate)
+            }
         }
     }
 
+    private fun launchBackAnimation(startX:Float, startY:Float){
+        isAnimationActive=true
+        ivCrystal.animate().apply {
+            duration = 1
+            translationX(startX)
+            translationY(startY)
+            scaleX(1F)
+            scaleY(1F)
+            withEndAction {
+                setupRandomCrystal()
+            }
+        }
+    }
 }
